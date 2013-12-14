9377c80111f9d3b7b61355f813dc5f16acdcee03
jeremy-denis <jeremy.denis9@hotmail.fr>
2013-12-14 19:56:06 +0100

    added coment in Finder and EntityFinder class to help understand

--- TEF/src/Database/EntityFinder.php
+++ TEF/src/Database/EntityFinder.php
@@ -1,5 +1,11 @@
 <?php
-
+/*************************************
+ * Title 			: 	EntityFinder.php
+ * Creation_date 	:	1/10/2013 
+ * Author 			: 	Jeremy DENIS
+ * Licence 			: 	php
+ * Description 		: 	class to find the Entity in the database
+ *************************************/
 namespace Database;
 
 use Database\Finder;
@@ -10,12 +16,21 @@ class EntityFinder implements Finder
 	private $driver;
 	private $entity;
 	
+	/**function __construct
+	 * Constructor of the entity finder
+	 * @param $driver the driver use to recover data
+	 * @param the entity concern by the finder
+	 */
 	public function __construct($driver, Entity $entity)
 	{
 		$this->driver = $driver;
 		$this->entity = $entity;
 	}
 	
+	/**function : findOneByID
+	 * @return one entity find by his id
+	 * @param $id the id of the entity to get
+	 */
 	public function findOneById($id)
 	{
 		$result = $this->driver->findOneById($id,Entity::toTableName($this->entity->getName()),$this->entity->getIdColumn());
@@ -31,6 +46,14 @@ class EntityFinder implements Finder
 		return null;
 	}
 
+	/**function : findAll
+	 * function to get all the entity in the database
+	 * @return a set of all th entity
+	 * @param $orderBy : the column use to order the set
+	 * @param $criteria : a key value array that match the result return
+	 * @param $revert : to choose if the order of the set is acendent or descendent
+	 * @param $where : the where clause of the request 
+	 */
 	public function findAll($orderBy = null,$criteria=null,$revert=0,$where = null)
 	{
 		$entities = $this->driver->findAll(Entity::toTableName($this->entity->getName()),$orderBy,$criteria,$revert,$where);
@@ -47,16 +70,28 @@ class EntityFinder implements Finder
 		return $arrayEntity;
 	}	
 	
+	/**function : getEntityName
+	 * function to get the name of the entity that match the finder 
+	 * @return the name of the entity table where the data are extract 
+	 */
 	public function getEntityName()
 	{
 		return $this->entity->getName();
 	}
 	
+	/**function : getIdColumn
+	 * function to get the name of the column where are saved the id of the entity in the database
+	 * @return the name of the id column of the entity
+	 */
 	public function getEntityTable()
 	{
 		return $this->entity->toTableName($this->entity->getName());
 	}
 	
+	/**function : getIdColumn
+	 * function to get the name of the column where are saved the id of the entity in the database
+	 * @return the name of the id column of the entity
+	 */
 	public function getColumnId()
 	{
 		return $this->entity->getIdColumn();
--- TEF/src/Database/Finder.php
+++ TEF/src/Database/Finder.php
@@ -1,11 +1,46 @@
 <?php
-
+/*************************************
+ * Title 			: 	Finder.php
+ * Creation_date 	:	1/10/2013 
+ * Author 			: 	Jeremy DENIS
+ * Licence 			: 	php
+ * Description 		: 	Interface that get the entities in the database
+ *************************************/
 namespace Database;
 
 interface Finder
 {
+	/**function : findOneByID
+	 * @return one entity find by his id
+	 * @param $id the id of the entity to get
+	 */
 	public function findOneById($id);
-	public function findAll();
+	
+	/**function : findAll
+	 * function to get all the entity in the database
+	 * @return a set of all th entity
+	 * @param $orderBy : the column use to order the set
+	 * @param $criteria : a key value array that match the result return
+	 * @param $revert : to choose if the order of the set is acendent or descendent
+	 * @param $where : the where clause of the request 
+	 */
+	public function findAll($orderBy = null,$criteria=null,$revert=0,$where = null);
+	
+	/**function : getEntityTable
+	 * function to get the table from the database where the entity is recover
+	 * @return the name of the table where the entity is recover
+	 */
 	public function getEntityTable();
+	
+	/**function : getIdColumn
+	 * function to get the name of the column where are saved the id of the entity in the database
+	 * @return the name of the id column of the entity
+	 */
 	public function getColumnId();
+	
+	/**function : getEntityName
+	 * function to get the name of the entity that match the finder 
+	 * @return the name of the entity table where the data are extract 
+	 */
+	public function getEntityName();
 }
